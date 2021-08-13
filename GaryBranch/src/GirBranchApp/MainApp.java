c61dae264937474a145bd2bc52dcc3c5466e6b36
LAPTOP-6IR4E4AF\Tibame_T14 <mmm912026@gmail.com>
2021-08-13 09:31:32 +0800

    add math 2222

--- GaryBranch/src/GirBranchApp/MainApp.java
+++ GaryBranch/src/GirBranchApp/MainApp.java
@@ -5,9 +5,9 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1st Version!!");
-		System.out.println("2se Version!!");
+		System.out.println("2sc Version!!");
 		//新加的類別
-		System.out.println(new Math().add(1,2));
+		System.out.println(new Math().add(1,3));
 	}
 
 }
