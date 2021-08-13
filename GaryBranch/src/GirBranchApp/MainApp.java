fc202985b2e024f4532e8a4ee5fcffdbf288e126
LAPTOP-6IR4E4AF\Tibame_T14 <mmm912026@gmail.com>
2021-08-13 09:34:59 +0800

    Branch add 3

--- GaryBranch/src/GirBranchApp/MainApp.java
+++ GaryBranch/src/GirBranchApp/MainApp.java
@@ -7,7 +7,7 @@ public class MainApp {
 		System.out.println("1st Version!!");
 		System.out.println("2sc Version!!");
 		//新加的類別
-		System.out.println(new Math().add(1,3));
+		System.out.println(new Math().add(10,5));
 	}
 
 }
