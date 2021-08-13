80584c4a71a8428c5a2b1531ec8b37d8576a1803
LAPTOP-6IR4E4AF\Tibame_T14 <mmm912026@gmail.com>
2021-08-13 09:26:41 +0800

    Add Math Branch

--- GaryBranch/src/GirBranchApp/MainApp.java
+++ GaryBranch/src/GirBranchApp/MainApp.java
@@ -5,6 +5,16 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1st Version!!");
+		System.out.println("2se Version!!");
+		//新加的類別
+		System.out.println(new Math().add(1,2));
 	}
 
+}
+
+
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
